23c9ee27cd02d70d097a8366c78841b2ea62ac6d
Fejiroofficial <houseofjiro@gmail.com>
2018-07-01 23:30:23 +0100

    Creating object store

--- js/idb-test/index.js
+++ js/idb-test/index.js
@@ -1,7 +1,6 @@
-//var idb = require ('idb');
+// var idb = require('idb');
 
 
-// var dpPromise = idb.open('converter-db', 1, function (upgradeDb) {
-//     var keyCurrencyStore =  upgradeDb.createObjectStore('keycurrency');
-//     keyCurrencyStore.put('value- 360', 'key NGA_USD');
-// });
+// const dpPromise = idb.open('converter-db', 1, function (upgradeDb) {
+//      var keyCurrencyStore =  upgradeDb.createObjectStore('Currency');
+//  });
