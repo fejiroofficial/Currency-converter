3832d3b08ee7eea89d1c84018f354e64b349da5f
Fejiroofficial <houseofjiro@gmail.com>
2018-07-01 22:44:56 +0100

    editing cache files

--- service-worker.js
+++ service-worker.js
@@ -34,7 +34,9 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function (event){
     event.respondWith(
         caches.match(event.request).then(function(response){
-            if(response) return response;
+            if(response) {
+                return response;
+            }
             return fetch (event.request);
         })
     )
